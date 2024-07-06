b1f28b924f89c8f682e5ca889f22639115162cdd
andrevmoreno <andreavalentinamoreno158@gmail.com>
2024-07-06 18:32:23 -0400

    Update Image

---
+++ Roting Planet/Planets.swift
@@ -0,0 +1,18 @@
+//
+//  Planets.swift
+//  Roting Planet
+//
+//  Created by Andrea Moreno on 03-07-24.
+//
+
+import SwiftUI
+
+struct Planets: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Planets()
+}
